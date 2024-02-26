861e064b5730771bd85ae5f5deb299233d116050
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2024-02-27 03:24:17 +0530

    completed the assignment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DashBoard from "./components/DashBoard";
+
+function App() {
+  return (
+    <>
+      <div className="p-10">
+        <DashBoard />
+      </div>
+    </>
+  );
+}
+
+export default App;
