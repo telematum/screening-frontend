8e1b6174769a077373bea89b787405129b00a874
unknown <shinchanmusic24@gmail.com>
2024-02-28 18:10:51 +0530

    text-size

--- src/components/Appointment_List.jsx
+++ src/components/Appointment_List.jsx
@@ -39,7 +39,7 @@ function Appointment_List() {
                 <h1 className='font-bold text-[34px] text-gray-500 mb-8'>Today's Appointment List</h1>
                 <table className=' w-full   '>
                     <thead className=' rounded-lg text-gray-400 bg-gray-100 '  >
-                        <tr className='text-left text-[20px] font-[300px] uppercase '>
+                        <tr className='text-left text-[19px] font-[300px] uppercase '>
                             <th className='px-4 py-2'>Patients</th>
                             <th className='px-4 py-2'>Date</th>
                             <th className='px-4 py-2'>Time</th>
@@ -54,32 +54,32 @@ function Appointment_List() {
                                 <td className='px-4 py-3'>
                                     <div className='flex items-center gap-2 mb-1'>
                                         <span style={{backgroundColor:getRandomColor()}} className='bg-${getRandomColor()} h-[28px] w-[28px] rounded-full'></span>
-                                        <span className='text-[19px] text-slate-600 font-bold'>{item.patient_name}</span>
+                                        <span className='text-[17px] text-slate-600 font-bold'>{item.patient_name}</span>
                                     </div>
-                                    <span className='text-[17px] font-semibold text-slate-700'>+{item.mobile_number}</span>
+                                    <span className='text-[16px] font-semibold text-slate-700'>+{item.mobile_number}</span>
                                 </td>
                                 <td className='px-4 py-2 '>
-                                    <div className='flex items-center gap-2 text-slate-500 font-semibold text-[19px]'>
+                                    <div className='flex items-center gap-2 text-slate-500 font-semibold text-[17px]'>
                                         <span><SlCalender  /></span>
 
                                         <span>{formatDate(item.appointment_date)}</span>
                                     </div>
                                 </td>
-                                <td className='px-4 py-2'><div className='flex items-center gap-2 text-slate-500 font-semibold text-[19px] '>
+                                <td className='px-4 py-2'><div className='flex items-center gap-2 text-slate-500 font-semibold text-[17px] '>
                                     <span><IoMdTime /></span>
 
                                     <span>{item.appointment_time}</span>
                                 </div> </td>
                                 <td className='px-4 py-2'>
-                                    <div className='flex items-center gap-2 text-slate-500 font-semibold text-[19px] '>
+                                    <div className='flex items-center gap-2 text-slate-500 font-semibold text-[17px] '>
                                         <span className='text-green-500'><MdStars /></span>
 
                                         <span>{item.doctor}</span>
                                     </div>
                                 </td>
-                                <td className='px-4 py-2'> <span className='bg-slate-200 rounded-lg px-3 py-2 font-bold text-[19px] text-slate-500 text-center'>{item.injury} </span></td>
+                                <td className='px-4 py-2'> <span className='bg-slate-200 rounded-lg px-3 py-2 font-bold text-[17px] text-slate-500 text-center'>{item.injury} </span></td>
                                 <td className='px-4 py-2'>
-                                    <span className=' cursor-pointer'><HiOutlineDotsVertical className='hover:scale-150 transition duration-300 ease-in-out text-[19px]'/></span>
+                                    <span className=' cursor-pointer'><HiOutlineDotsVertical className='hover:scale-150 transition duration-300 ease-in-out text-[17px]'/></span>
                                 </td>
                             </tr>
                         ))}
