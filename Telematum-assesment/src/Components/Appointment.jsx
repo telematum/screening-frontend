e50c805804660d7647b522ccd4c2e8138dc8fe1d
Onkar Karale <onkarkarale4@gmail.com>
2024-03-01 01:12:06 +0530

    Completed the Assessment

--- Telematum-assesment/src/Components/Appointment.jsx
+++ Telematum-assesment/src/Components/Appointment.jsx
@@ -30,31 +30,31 @@ const Appointment = () => {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center h-[100vh] mx-auto'>
-      <div className='border p-10 rounded-3xl'>
-        <h1 className='text-2xl text-gray-500 font-bold'>Today's Appointment List</h1>
-        <div className='mt-5'>
+    <div className='flex flex-col items-center justify-center min-h-screen mx-auto px-4'>
+      <div className='border p-4 rounded-lg max-sm:w-full  lg:max-w-screen-lg max-w-screen-md'>
+        <h1 className='text-2xl text-gray-500 font-bold mb-4'>Today's Appointment List</h1>
+        <div className='max-sm:overflow-x-auto'>
           {
             loading ? (
-              <p>Loading the Data...</p>
+              <p className='text-red-600 text-center font-bold'>Loading the Data...</p>
             ) : (
-              <table>
-                <thead className='border-b '>
-                  <tr className='uppercase text-gray-400'>
-                    <th className='py-3 '>Patient</th>
+              <table className='w-full whitespace-nowrap'>
+                <thead>
+                  <tr className='text-gray-400 uppercase'>
+                    <th className='py-3'>Patient</th>
                     <th>Date</th>
                     <th>Time</th>
                     <th>Doctor</th>
                     <th>Injury</th>
                     <th>Action</th>
                   </tr>
-                </thead >
+                </thead>
                 <tbody>
                   {appointments.map((appointment, index) => (
                     <Details key={index} info={appointment} />
                   ))}
                 </tbody>
-              </table >
+              </table>
             )
           }
         </div>
