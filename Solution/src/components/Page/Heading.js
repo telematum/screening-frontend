f7c3cad997532113546406ece428d8a792f3f1f5
Vaishali <aggarwal.vaishali785@gmail.com>
2024-03-01 15:07:17 +0530

    updated image of final result

--- Solution/src/components/Page/Heading.js
+++ Solution/src/components/Page/Heading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = () => {
     return (
-        <h2 className='font-semibold text-lg text-[var(--theme-text-color)] mb-4'>Today's Appointment List</h2>
+        <h2 className='font-semibold text-xl text-[var(--theme-text-color)] mb-4'>Today's Appointment List</h2>
     )
 }
 
