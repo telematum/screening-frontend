9714085ce3a4b16c4db3d85efba216d13e99f110
abbas414 <abbasaliaatir@gmail.com>
2024-03-02 03:21:30 +0530

    feat: addded dashboard listing with api calling

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import DashboardListing from './components/DashboardListing';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <DashboardListing/>
+     
+
     </div>
   );
 }
