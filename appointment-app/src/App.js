edbcdcf0bbe0f1f82ac32fd3b190379e4059e561
pawanpokhriyal1 <pokhriyal.pawan.1407@gmail.com>
2024-03-02 18:50:08 +0530

    added assignment changes

---
+++ appointment-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppointmentList from './AppointmentList';
+
+function App() {
+  return (
+    <h1 className=" flex-row justify-start ml-20 size-11/12 border-2 border-slate-50 rounded-sm shadow-md">
+      <AppointmentList />
+    </h1>
+  );
+}
+
+export default App;
